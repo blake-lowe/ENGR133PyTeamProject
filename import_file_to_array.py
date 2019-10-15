b0c3938ea1ef3b159277abf69abee311ecc435fb
chrislevy14 <chrislevy360@gmail.com>
2019-10-15 11:13:01 -0400

    Added import file to array script and added test function in main

---
+++ import_file_to_array.py
@@ -0,0 +1,9 @@
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+def importImage(path):
+    image = mpimg.imread(path) #Where path is the file path as a string
+    return image
+
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("hello world")
-print("Blake isn't gay, he's just homiesexual.")
-print("^facts")
-print("big facts")
+
+
+import import_file_to_array as ifta
+filepath = "/Users/G/Documents/ENGR 133/Group Python/boys.png"
+meNDaBoyz = ifta.importImage(filepath)
+print(meNDaBoyz)
