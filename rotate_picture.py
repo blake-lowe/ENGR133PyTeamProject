7141eae595b39fcd2ac17b66f473e6c4e7678958
mlannie <marcushlannie@gmail.com>
2019-10-17 10:21:14 -0400

    updated rotate

---
+++ rotate_picture.py
@@ -0,0 +1,10 @@
+def rotate(image):
+    h,w,c = image.shape
+
+    empty_image = np.zeros([h,w,c], dtype=np.uint8)
+
+    for i in range(h):
+        for j in range(w):
+            empty_image[i, j] = image[j-1, i-1]
+    
+    return(empty_image)
