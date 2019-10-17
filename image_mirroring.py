5a6d8b0b8e747e566b034a58b64d63ddea095a65
chrislevy14 <chrislevy360@gmail.com>
2019-10-17 10:39:19 -0400

    stuff

--- image_mirroring.py
+++ image_mirroring.py
@@ -1,8 +1,14 @@
 import numpy as np
-import matplotlib as mp
+import matplotlib.image as mpimg
 
-def mirror(path):
-    image = mp.image.imread(path)
-    flipped = np.fliplr(image)
-    return flipped
-    
+def mirrorH(path):
+    image = mpimg.imread(path)
+    x=0
+    for i in image:
+        for j in range(len(image[0])+1,-1):
+            image[i][x]
+            x+=1
+            if x > len(image[i]):
+                x=0
+    return image
+
