97613851fa92d7371b9592b88a81178340674d78
tmweese <56595015+tmweese@users.noreply.github.com>
2019-10-17 10:45:19 -0400

    reeeee

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import import_file_to_array as ifta
 import image_mirroring as im
+import arraytoimage as ati
 import os
-
+name = input('Input output file name: ')
 
 ## TEST IMPORT FILE TO ARRAY
 filepath = os.getcwd()+ "/boys.png"
@@ -9,5 +10,8 @@ meNDaBoyz = ifta.importImage(filepath)
 print(meNDaBoyz)
 
 ##MIRROR IMAGE
-mirrored = im.mirror(filepath)
+mirrored = im.mirrorH(filepath)
 print(mirrored)
+
+## OUTPUT FILE
+ati.outimage(name, mirrored)
