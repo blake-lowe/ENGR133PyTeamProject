f6781040eef5f7b0a4ee8c57c3cc20038f5fa576
chrislevy14 <chrislevy360@gmail.com>
2019-10-17 09:33:28 -0400

    Not much

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import import_file_to_array as ifta
 
-filepath = "/Users/G/Documents/ENGR 133/Group Python/boys.png"
 
+## TEST IMPORT FILE TO ARRAY
+filepath = "/Users/G/Documents/ENGR 133/Group Python/boys.png"
 meNDaBoyz = ifta.importImage(filepath)
-print(meNDaBoyz)
+print(meNDaBoyz) 
