90bfaa2a62dca64ef3740fa6a52000269065e780
mlannie <marcushlannie@gmail.com>
2019-10-17 10:51:25 -0400

    blakeness

--- rotate_picture.py
+++ rotate_picture.py
@@ -2,7 +2,7 @@ import numpy as np
 def rotate(image):
     h,w,c = image.shape
 
-    empty_image = np.zeros([h,w,c], dtype=np.uint8)
+    empty_image = np.zeros([w,h,c]) #dtype=np.uint8
 
     for i in range(h):
         for j in range(w):
