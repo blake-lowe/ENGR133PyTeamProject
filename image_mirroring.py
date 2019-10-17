1dceb4322970b68ffd9463e84e26bab91420c2a2
chrislevy14 <chrislevy360@gmail.com>
2019-10-17 14:44:51 -0400

    fuck this

--- image_mirroring.py
+++ image_mirroring.py
@@ -5,10 +5,9 @@ import matplotlib.image as mpimg
 def mirrorH(path):
     image = mpimg.imread(path)
     outimage = image
-    for i in range(len(image)):
-        for j in range(len(image[i])):
+    for i in range(0,len(image)):
+        for j in range(0,len(image[i])//2):
             outimage[i][j] = image[i][-j]
+        for j in range():
             outimage[i][-j] = image[i][j]
-    
     return outimage
-
