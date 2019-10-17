373ee072940a2d109259e1251048c95d62440752
chrislevy14 <chrislevy360@gmail.com>
2019-10-17 09:56:30 -0400

    some stuffs

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import import_file_to_array as ifta
+import os
 
 
 ## TEST IMPORT FILE TO ARRAY
-filepath = "/Users/G/Documents/ENGR 133/Group Python/boys.png"
+filepath = os.getcwd()+ "/boys.png"
 meNDaBoyz = ifta.importImage(filepath)
 print(meNDaBoyz)
