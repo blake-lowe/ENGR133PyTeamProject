8f73de4fa06fe8977c9c8f616304ec48972c32f4
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2019-10-22 09:42:44 -0400

    blur initial framework

--- blur.py
+++ blur.py
@@ -1,18 +1,43 @@
 
 import math
-def getKernel(size, stdev):#odd integer size of square kernel, spread in both directions
+
+
+def process(imageData):
+    return
+
+def getKernel(size, stddev):#odd integer size of square kernel, spread in both directions
     kernel = []
 
-    center = (int(size)/2)
+    center = (int(size))
     for j in range(0, size):
+        row = []
         for i in range(0, size):
-            row = []
-            row.append(#todo)
-
+            x = i-center
+            y = j-center
+            row.append(gaussian2D(x, y, stddev))
+        kernel.append(row)
+    kernel = normalizeArray(kernel)
     return kernel
 
 def gaussian2D(x, y, stddev):
     A = 1/(2*math.pi*stddev*stddev)
     return A*math.exp(-(x*x+y*y)/(2*stddev*stddev))
 
+
+def normalizeArray(array):#2D array input
+    total = 0
+    for i in range(len(array[0])):
+        for j in range(len(array)):
+            total += array[i][j]
+    #print(sum(sum(array,[])))
+    for i in range(len(array[0])):
+        for j in range(len(array)):
+            array[i][j] /= total
+    
+    #print(sum(sum(array,[])))
+    return(array)
+
+
+
+print(getKernel(3, 5.5))
 print(gaussian2D(0, 0, 1))
