c633610f66a819b9b1f26571539e242aa93a21e1
chrislevy14 <chrislevy360@gmail.com>
2019-10-22 10:32:31 -0400

    Grayscale

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import import_file_to_array as ifta
 import image_mirroring as im
 import arraytoimage as ati
 import rotate_picture as rp
+import grayscale as gs
 import os
 name = input('Input output file name: ')
 
@@ -11,11 +12,13 @@ meNDaBoyz = ifta.importImage(filepath)
 #print(meNDaBoyz)
 
 ## MIRROR IMAGE
-mirrored = im.flipV(meNDaBoyz)
+#mirrored = im.flipV(meNDaBoyz)
 #print(mirrored)
 
 ## ROTATE IMAGE
 #rotated = rp.rotate(meNDaBoyz)
 
+## GRAYSCALE
+gray = gs.makeGray(meNDaBoyz)
 ## OUTPUT FILE
-ati.outimage(name, mirrored)
+ati.outimage(name, gray)
