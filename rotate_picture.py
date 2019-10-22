6fc175303c24b55f9b48040abd72793b8fd7c302
mlannie <marcushlannie@gmail.com>
2019-10-22 10:10:33 -0400

    Update rotate_picture.py

--- rotate_picture.py
+++ rotate_picture.py
@@ -1,21 +1,11 @@
 import numpy as np
-def rotate(image):
-    h,w,c = image.shape
+def rotate(image): #ROTATES IMAGE 90 DEGREES COUNTERCLOCKWISE
+    h,w,c = image.shape #INTIALIZES VARIABLES FOR THE HEIGHT, WIDTH, AND COLORS OF THE SHAPE
 
-    empty_image = np.zeros([w,h,c]) #,dtype=np.uint8
+    empty_image = np.zeros([w,h,c]) #CREATES A NEW 0 MATRIX WITH WIDTH AND HEIGHT OPPOSITE TO THE ORIGINAL IMAGE
 
     for i in range(h):
         for j in range(w):
-            empty_image[j, i] = image[i, j-1]
+            empty_image[j, i] = image[i, j-1] #ROTATES EACH PIXEL 90 DEGREES COUNTERCLOCKWISE
     
     return(empty_image)
-
-
-    ''' int m = input[0].length();
-    int [][] output = new int [m][n];
-
-    for (i=0; i<n; i++)
-	    for (int j=0;j<m; j++)
-		    output [j][n-1-i] = input[i][j];
-    
-    return output; '''
