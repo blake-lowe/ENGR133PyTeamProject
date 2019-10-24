369cb2644c6fa95d3cc2e485036d321883ceeacf
mlannie <marcushlannie@gmail.com>
2019-10-24 10:37:04 -0400

    Update rotate_picture.py

--- rotate_picture.py
+++ rotate_picture.py
@@ -1,3 +1,26 @@
+'''
+===============================================================================
+ENGR 133 Program Description 
+	This function rotates an image 90 degrees counterclockwise
+
+Assignment Information
+	Assignment:     Py4_PA_Task1
+	Author:         Marcus Lannie, mlannie@purdue.edu
+	Team ID:        002-10
+	
+Contributor:    None, name@purdue.edu [repeat for each]
+	My contributor(s) helped me:	
+	[ ] understand the assignment expectations without
+		telling me how they will approach it.
+	[ ] understand different ways to think about a solution
+		without helping me plan my solution.
+	[ ] think through the meaning of a specific error or
+		bug present in my code without looking at my code.
+	Note that if you helped somebody else with their code, you
+	have to list that person as a contributor here as well.
+===============================================================================
+'''
+
 import numpy as np
 def rotate1(image): #ROTATES IMAGE 90 DEGREES COUNTERCLOCKWISE
     h,w,c = image.shape #INTIALIZES VARIABLES FOR THE HEIGHT, WIDTH, AND COLORS OF THE SHAPE
